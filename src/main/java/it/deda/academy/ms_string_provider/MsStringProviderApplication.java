fe9b1b6943fc662040fd89b344deff35abeb9af5
Viorel Strogoteanu <viorelss@icloud.com>
2024-08-02 09:52:30 +0200

    Project initialization

---
+++ src/main/java/it/deda/academy/ms_string_provider/MsStringProviderApplication.java
@@ -0,0 +1,13 @@
+package it.deda.academy.ms_string_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStringProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStringProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/deda/academy/ms_string_provider/MsStringProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package it.deda.academy.ms_string_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStringProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
